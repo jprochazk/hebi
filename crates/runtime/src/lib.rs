42fc59302a463a3b06286cd6f0666d195f1cdaf4
jprochazk <1665677+jprochazk@users.noreply.github.com>
2023-02-12 21:22:56 +0100

    todos

--- crates/runtime/src/lib.rs
+++ crates/runtime/src/lib.rs
@@ -2,5 +2,23 @@ pub mod isolate;
 mod util;
 pub mod value;
 
+/*
+TODO: some cleanup after the vm+value merge
+
+- `util`
+- `error`
+
+finish the object access API
+
+
+TODO: carefully design the public API
+- Value
+  - constructors
+  - as_*
+- Isolate
+  - call
+  - ?
+*/
+
 pub use isolate::{Error, Isolate};
 pub use value::Value;
