20be10fbb50ab77a04580ec9e496d4a6fd237364
jprochazk <1665677+jprochazk@users.noreply.github.com>
2023-04-29 21:26:14 +0200

    diassemble with offsets

--- src/bytecode/disasm.rs
+++ src/bytecode/disasm.rs
@@ -1,7 +1,7 @@
 use std::fmt::Display;
 
 use super::opcode::symbolic;
-use crate::util::JoinIter;
+use crate::util::{num_digits, JoinIter};
 use crate::value::constant::Constant;
 
 pub struct Instruction<'a> {
@@ -37,14 +37,16 @@ pub struct Disassembly<'a> {
   bytecode: &'a [u8],
   constants: &'a [Constant],
   padding: usize,
+  offsets: bool,
 }
 
 impl<'a> Disassembly<'a> {
-  pub fn new(bytecode: &'a [u8], constants: &'a [Constant], padding: usize) -> Self {
+  pub fn new(bytecode: &'a [u8], constants: &'a [Constant], padding: usize, offsets: bool) -> Self {
     Self {
       bytecode,
       constants,
       padding,
+      offsets,
     }
   }
 }
@@ -52,16 +54,30 @@ impl<'a> Disassembly<'a> {
 impl<'a> Display for Disassembly<'a> {
   fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
     let mut current_remainder = self.bytecode;
+    let mut offset = 0;
+    let offset_width = num_digits(self.bytecode.len());
     while !current_remainder.is_empty() {
       let (instruction, remainder) = symbolic::decode(current_remainder).ok_or(std::fmt::Error)?;
+      let size = (remainder.as_ptr() as usize) - (current_remainder.as_ptr() as usize);
       current_remainder = remainder;
-      writeln!(
-        f,
-        "{:padding$}{}",
-        "",
-        instruction.disassemble(self.constants),
-        padding = self.padding
-      )?;
+      if self.offsets {
+        writeln!(
+          f,
+          "{:padding$}{offset: <offset_width$} | {}",
+          "",
+          instruction.disassemble(self.constants),
+          padding = self.padding
+        )?;
+      } else {
+        writeln!(
+          f,
+          "{:padding$}{}",
+          "",
+          instruction.disassemble(self.constants),
+          padding = self.padding
+        )?;
+      }
+      offset += size;
     }
     Ok(())
   }
