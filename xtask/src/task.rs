d9395c5aeb2e09442988685b6c9d802a35716b5b
jprochazk <1665677+jprochazk@users.noreply.github.com>
2023-05-19 22:49:02 +0200

    xtask documentation

--- xtask/src/task.rs
+++ xtask/src/task.rs
@@ -1,22 +1,5 @@
 use crate::Result;
 
-pub mod common;
-
-pub mod examples;
-pub mod miri;
-pub mod snap;
-pub mod test_all;
-
-pub fn run(which: &str, args: &[String]) -> Result<()> {
-  match which {
-    "examples" => examples::run(args),
-    "snap" => snap::run(args),
-    "miri" => miri::run(args),
-    "test-all" => test_all::run(args),
-    _ => print_help(),
-  }
-}
-
 const HELP: &str = "\
 Tasks:
   examples : run all examples
@@ -25,7 +8,26 @@ Tasks:
   test-all : run tests and examples
 ";
 
+pub mod common;
+
+pub mod examples;
+pub mod miri;
+pub mod snap;
+pub mod template;
+pub mod test_all;
+
 pub fn print_help() -> Result<()> {
   eprint!("{HELP}");
   Ok(())
+}
+
+pub fn run(which: &str, args: &[String]) -> Result<()> {
+  match which {
+    "examples" => examples::run(args),
+    "snap" => snap::run(args),
+    "miri" => miri::run(args),
+    "test-all" => test_all::run(args),
+    // "template" => template::run(args),
+    _ => print_help(),
+  }
 }
---
+++ xtask/src/task/template.rs
@@ -0,0 +1,9 @@
+//! Task template
+//!
+//! Copy it over to a new file to bootstrap a new task.
+
+use crate::Result;
+
+pub fn run(args: &[String]) -> Result<()> {
+  todo!("Template command")
+}
