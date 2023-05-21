644870d962f4be29f8c122ba86085fdd7085387e
jprochazk <1665677+jprochazk@users.noreply.github.com>
2023-05-21 14:08:22 +0200

    remove `__miri` feature flag this enables usage of `--all-features`

--- src/lib.rs
+++ src/lib.rs
@@ -21,3 +21,31 @@ mod vm;
 
 pub mod public;
 pub use public::*;
+
+/*
+- [x] remove `__miri` feature and put args in `xtask miri` instead (`--filter` for running only specific tests)
+  - should unlock `--all-features`
+- [ ] change ptr repr to use manual vtable + static object trait
+- [ ] vm fully async
+  - [x] stackless import
+  - [ ] stackless class init
+    - [ ] make `init` a contextual keyword that doesn't require `fn` in classes
+    - [ ] user cannot access initializer (to call it again)
+          only way is to call the class type
+    - [ ] remove `super.init()` and replace with calling the proxy (`super()`)
+- [ ] fix `from m import a, b, c` bug
+- [ ] rename internal `hebi::String` to `hebi::Str`
+- [ ] remove `Ref` from name of public value types (inner should be prefix by `Owned` or qualified path)
+- [ ] fix `scope.params` will panic if given the wrong number of args
+- [ ] change `This` to support any value (?)
+
+- [ ] for iter loops
+- [ ] generators
+- [ ] list indexing
+- [ ] f-strings
+- [ ] ops on builtins
+- [ ] methods on builtins
+- [ ] timeout feature (abort signal, Arc<AtomicBool> or similar)
+- [ ] semicolons (`;` for stmt, `;;` for block)
+- [ ] report code locations (intern spans, track in hashmap of `offset->span_id`)
+*/
--- xtask/src/task/miri.rs
+++ xtask/src/task/miri.rs
@@ -6,8 +6,10 @@ const MIRIFLAGS: &str = "-Zmiri-tree-borrows -Zmiri-permissive-provenance";
 pub fn run(args: &[String]) -> Result<()> {
   cargo("miri")
     .env("MIRIFLAGS", MIRIFLAGS)
-    .args(["-F", "serde"])
     .args(args.iter())
+    .args(["--no-default-features", "-F", "nanbox", "-F", "serde"])
+    .args(["--"])
+    .args(["serde", "util", "value", "vm", "ptr"])
     .spawn()?
     .wait()?
     .check()
